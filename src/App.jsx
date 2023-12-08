a23babd0e55280c955a6517ccd0375ef86053e7f
Logan Gibler <logan.gibler@gmail.com>
2023-12-08 18:56:00 -0500

    just getting started

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import "./App.css";
+import { Nav } from "./components";
 
 function App() {
   return (
-    <>
-      <h1>Hello new Project!</h1>
-    </>
+    <div>
+      <Nav />
+    </div>
   );
 }
 
---
+++ src/components/Nav.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Nav = () => {
+  return <div className="h-100 bg-black">Hello</div>;
+};
+
+export default Nav;
